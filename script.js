f9510839ea3718ef59809feafa1b76f4f96a6657
Ganesh Ardalkar <37583773+ganeshardlkar@users.noreply.github.com>
2022-12-24 18:30:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function addNewUser() {
     var newuserprof = document.getElementById("newUserProf").value;
     var newuserage = document.getElementById("newUserAge").value;
     if(newusername == '' || newuserprof == '' || newuserage == '') {
-        console.log("Please enter all details");
+        alert("Please enter all details");
         return false;
     }
     count++;
